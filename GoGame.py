c4745a21669059e37d86e9c242fc66558937dd82
LuskasCarneiro <alexandrelusquinhos@gmail.com>
2023-12-04 17:11:30 +0000

    Cleanup do codigo same defects :(

--- GoGame.py
+++ GoGame.py
@@ -13,7 +13,7 @@ class GoGame:
         self.tabuleirobefore=self.tabuleiro.copy()
 
     def remove(self,x,y):
-        self.tabuleiro[x][y]=0
+        self.tabuleiro[x][y]=values.default
 
     def get_liberties(self,x,y):
         liberties_vector=[]
@@ -31,13 +31,13 @@ class GoGame:
     
     def liberties_are_equal(self,x,y):
         liberties=self.get_liberties(x,y)
-        print(f"Ponto possivelmente afetado x:{x} y:{y}, Liberties:{liberties}")
+
         if(liberties[0]==values.default):
-            #para lidar com as situacoes onde uma peca e rodeada de 0's, podemos retornar logo 0 porque se forem todos 0 e falso e se tiver pelo menos um zero e logo falso poque nao da para capturar e nao
-            print("Liberties com pelo menos um 0")
+            #para lidar com as situacoes onde uma peca e rodeada de 0's, 
+            #podemos retornar logo 0 porque se forem todos 0 e falso e se tiver pelo 
+            #menos um zero e logo falso poque nao da para capturar e nao
             return False
-        print(all(i == liberties[0] for i in liberties))
-        print(all(i == liberties[0] for i in liberties) and (self.tabuleiro[x][y] != liberties[0]))
+        
         if (all(i == liberties[0] for i in liberties) and (self.tabuleiro[x][y] != liberties[0])):
             return True
         else: 
@@ -45,22 +45,18 @@ class GoGame:
     
     def capture(self,x,y):
         if self.liberties_are_equal(x,y):
-            self.tabuleiro[x][y]=values.default
+            self.remove(x,y)
 
     def check_adjacent_captures(self,x,y):
-        if ((x-1)>=0) and (self.tabuleiro[x-1][y]!=values.default):
+        if ((x-1)>=0) and (not self.isfree(x-1,y)):
             self.capture(x-1,y)
-            print("ajacente 1")
-        if ((x+1)<self.tabuleiro_size) and (self.tabuleiro[x+1][y]!=values.default):
+        if ((x+1)<self.tabuleiro_size) and (not self.isfree(x+1,y)):
             self.capture(x+1,y)
-            print("ajacente 2")
 
-        if ((y+1)<self.tabuleiro_size) and (self.tabuleiro[x][y+1]!=values.default):
+        if ((y+1)<self.tabuleiro_size) and (not self.isfree(x,y+1)):
             self.capture(x,y+1)
-            print("ajacente 3")
-        if ((y-1)>=0) and (self.tabuleiro[x][y-1]!=values.default):
+        if ((y-1)>=0) and (not self.isfree(x,y-1)):
             self.capture(x,y-1)
-            print("ajacente 4")
 
 
     def value(self):
@@ -69,9 +65,6 @@ class GoGame:
         else:
             return 2
         
-    def liberties(self,x,y):
-        return 0
-    
     def make_move(self,x,y):
         if self.isfree(x,y):
             tabuleiro_com_move=self.tabuleiro.copy()
@@ -113,22 +106,22 @@ class GoGame:
         self.tabuleiro[x][y]=self.value()
 
     def isfree(self,x,y):
-        return self.tabuleiro[x][y]==0
+        return self.tabuleiro[x][y]==values.default
     
     def isfull(self):
-        x,y=np.where(self.tabuleiro==0)
+        x,y=np.where(self.tabuleiro==values.default)
         if len(x)>0:
             return False
         return True
     
     def isempty(self):
-        x,y=np.where(self.tabuleiro==0)
+        x,y=np.where(self.tabuleiro==values.default)
         if len(x)!=(values.grid_size**2):
             return False
         return True
     
     def ocuppied_pos(self):
-        return np.where(self.tabuleiro!=0)
+        return np.where(self.tabuleiro!=values.default)
     
     def return_tabuleiro(self):
         return self.tabuleiro
