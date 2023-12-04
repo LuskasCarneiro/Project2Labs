6b4d34d804341f445f78be60b48d6e0cd3cd0ef7
LuskasCarneiro <alexandrelusquinhos@gmail.com>
2023-12-04 16:44:06 +0000

    Falta nao voltar ao estado anterior e o scoring

--- GoGame.py
+++ GoGame.py
@@ -15,6 +15,54 @@ class GoGame:
     def remove(self,x,y):
         self.tabuleiro[x][y]=0
 
+    def get_liberties(self,x,y):
+        liberties_vector=[]
+        if ((x-1)>=0):
+            liberties_vector.append(self.tabuleiro[x-1][y])
+        if ((x+1)<self.tabuleiro_size):
+            liberties_vector.append(self.tabuleiro[x+1][y])
+
+        if ((y+1)<self.tabuleiro_size):
+            liberties_vector.append(self.tabuleiro[x][y+1])
+        if ((y-1)>=0):
+            liberties_vector.append(self.tabuleiro[x][y-1])
+
+        return liberties_vector
+    
+    def liberties_are_equal(self,x,y):
+        liberties=self.get_liberties(x,y)
+        print(f"Ponto possivelmente afetado x:{x} y:{y}, Liberties:{liberties}")
+        if(liberties[0]==values.default):
+            #para lidar com as situacoes onde uma peca e rodeada de 0's, podemos retornar logo 0 porque se forem todos 0 e falso e se tiver pelo menos um zero e logo falso poque nao da para capturar e nao
+            print("Liberties com pelo menos um 0")
+            return False
+        print(all(i == liberties[0] for i in liberties))
+        print(all(i == liberties[0] for i in liberties) and (self.tabuleiro[x][y] != liberties[0]))
+        if (all(i == liberties[0] for i in liberties) and (self.tabuleiro[x][y] != liberties[0])):
+            return True
+        else: 
+            return False
+    
+    def capture(self,x,y):
+        if self.liberties_are_equal(x,y):
+            self.tabuleiro[x][y]=values.default
+
+    def check_adjacent_captures(self,x,y):
+        if ((x-1)>=0) and (self.tabuleiro[x-1][y]!=values.default):
+            self.capture(x-1,y)
+            print("ajacente 1")
+        if ((x+1)<self.tabuleiro_size) and (self.tabuleiro[x+1][y]!=values.default):
+            self.capture(x+1,y)
+            print("ajacente 2")
+
+        if ((y+1)<self.tabuleiro_size) and (self.tabuleiro[x][y+1]!=values.default):
+            self.capture(x,y+1)
+            print("ajacente 3")
+        if ((y-1)>=0) and (self.tabuleiro[x][y-1]!=values.default):
+            self.capture(x,y-1)
+            print("ajacente 4")
+
+
     def value(self):
         if self.turn:
             return 1
@@ -31,17 +79,16 @@ class GoGame:
             if self.legalmove(tabuleiro_com_move):
                 self.tabuleirobefore=self.tabuleiro
                 self.place(x,y)
+                self.check_adjacent_captures(x,y)
+                self.capture(x,y)
                 self.changeplayerturn()
                 self.print_tabuleiro()
-        
+
     def changeplayerturn (self):
         self.turn = not self.turn
 
     def legalmove(self,tabuleiro_com_move):
         return not np.array_equal(tabuleiro_com_move, self.tabuleirobefore)
-    
-    def iscaptured(self,x,y):
-        a=(self.tabuleiro)
 
     def inrange(self,x,y):
         if x<0:
--- values.py
+++ values.py
@@ -1,4 +1,5 @@
 canva_size=600
 color1='black'
 color2='blue'
+default=0
 grid_size=5
