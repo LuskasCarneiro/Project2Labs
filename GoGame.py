a5a8978d5386678d695a263a24405361e346afe2
LuskasCarneiro <alexandrelusquinhos@gmail.com>
2023-12-05 19:38:27 +0000

    fim da base

--- GoGame.py
+++ GoGame.py
@@ -7,6 +7,8 @@ class GoGame:
         self.tabuleiro_size=tabuleiro_size
         self.turn=True
         self.make_default_tabuleiro(tabuleiro_size)
+        self.score_black=0
+        self.score_white=7.5
         #self.make_fila_jogadas_default()
 
     def make_default_tabuleiro(self,tabuleiro_size):
@@ -96,6 +98,80 @@ class GoGame:
         #print(self.tabuleiroantes)
         #print("---------tabuleiro antes-----------")
         return not np.array_equal(tabuleiro_com_move, self.tabuleiroantes)
+    
+    ####-------------------------------------------- Testar se funciona------------------------------------------####
+    def returnWinner(self):
+        self.territorio()
+        print(f"score black:{self.score_black} score blue: {self.score_white}")
+
+        
+    def lib_territorios(self,coor):
+        neigh=[]
+        for element in [values.EAST,values.SOUTH,values.WEST,values.NORTH]:
+            pos=(coor[0]+element[0],coor[1]+element[1])
+            if(0<=pos[0]<values.grid_size and 0<=pos[1]<values.grid_size):
+                neigh.append(pos) 
+            
+        return neigh
+    
+    def territorio(self):
+        self.visited=[]
+        for i in range(values.grid_size):
+            for j in range(values.grid_size):
+                if(self.tabuleiro[i][j]==0):
+                    neigh=self.lib_territorios((i,j))
+                    if(not any(np.array_equal((i,j), item) for item in self.visited)):
+                        self.visited.append((i,j))
+                        self.cont_terr(neigh)
+                    
+    def cont_terr(self,neigh):
+        stack=list()
+        c=1
+        flagBlack=False
+        flagWhite=False
+        for space in neigh:
+            stack.append(space)
+            while(stack):
+                stone=stack.pop()
+                
+                if(self.tabuleiro[stone[0]][stone[1]]==0 and not any(np.array_equal(stone, item) for item in self.visited)):
+                    neigh=self.lib_territorios(stone)
+                    c+=1
+                    self.visited.append(stone)
+                    stack.extend(neigh)       # adicionar os vizinhos todos à lista
+                elif(self.tabuleiro[stone[0]][stone[1]]==1):
+                    flagBlack=True
+                elif(self.tabuleiro[stone[0]][stone[1]]==2):
+                    flagWhite=True
+                    
+        if(flagWhite and flagBlack):
+            return 
+        elif(flagBlack):
+            self.score_black+=c
+        elif(flagWhite):
+            self.score_white+=c
+            
+      ####-----------------------------------------------------------------------------------------####
+
+
+    """def flood_fill(self, x ,y, old, new):
+        # we need the x and y of the start position, the old value,
+        # and the new value
+        # the flood fill has 4 parts
+        # firstly, make sure the x and y are inbounds
+        if x < 0 or x >= len(values.grid_size) or y < 0 or y >= len(values.grid_size):
+            return
+        # secondly, check if the current position equals the old value
+        if self.tabuleiro[y][x] != old:
+            return
+
+        # thirdly, set the current position to the new value
+        self.tabuleiro[y][x] = new
+        # fourthly, attempt to fill the neighboring positions
+        self.flood_fill(x+1, y, old, new)
+        self.flood_fill(x-1, y, old, new)
+        self.flood_fill(x, y+1, old, new)
+        self.flood_fill(x, y-1, old, new)"""
 
     def inrange(self,x,y):
         if x<0:
--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import values
 from interface import *
 from tkinter import *
 
+i=10
 jogo=GoGame(values.grid_size)
-while True:
+while i>0:
     x,y = map(int, input("x, y = ").split())
     jogo.make_move(x,y)
+    i-=1
+jogo.returnWinner()
     
